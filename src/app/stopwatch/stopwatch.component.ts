eb5d24504c58f9b79d74458707d67bf816e42aaf
annaighoinart <evilicekinggunter@gmail.com>
2020-07-05 10:20:59 +0300

    rainbow mode + code cleaning

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -12,6 +12,8 @@ export class StopwatchComponent implements OnInit {
 
   isRunning: boolean = true
 
+  isRainbow: boolean = false
+
   time_stamp: number = 1000
 
   seconds: number = 0
@@ -22,7 +24,28 @@ export class StopwatchComponent implements OnInit {
   minutes_str: string = "00"
   hours_str: string = "00"
 
-  stopwatch;
+  stopwatch
+
+
+
+  // dry functions
+  reset_variables() {
+    this.seconds = 0;
+    this.minutes = 0;
+    this.hours = 0;
+  }
+
+  convert_time() {
+    this.seconds < 10 ? this.seconds_str = "0"+this.seconds : this.seconds_str = ""+this.seconds
+    this.minutes < 10 ? this.minutes_str = "0"+this.minutes : this.minutes_str = ""+this.minutes
+    this.hours < 10 ? this.hours_str = "0"+this.hours : this.hours_str = ""+this.hours
+  }
+  // dry functions end
+
+
+
+  //  int main(argc, **argv)      (here the timer ticks every N milliseconds, 'stopwatch' is the timer variable,
+  //  this is needed for proper restarting with another interval)
 
   ngOnInit() {
     this.stopwatch = timer(0, this.time_stamp).subscribe(ellapsedCycles => {
@@ -39,67 +62,56 @@ export class StopwatchComponent implements OnInit {
         }
 
 
-        this.seconds < 10 ? this.seconds_str = "0"+this.seconds : this.seconds_str = ""+this.seconds
-        this.minutes < 10 ? this.minutes_str = "0"+this.minutes : this.minutes_str = ""+this.minutes
-        this.hours < 10 ? this.hours_str = "0"+this.hours : this.hours_str = ""+this.hours
+        this.convert_time()
+
       }
     });
   }
 
+
+  // Start|Stop button handler (resets all the variables, then switches its state)
   toggleTimer() {
     if (this.isRunning) {
-      this.seconds = 0;
-      this.minutes = 0;
-      this.hours = 0;
-      this.seconds < 10 ? this.seconds_str = "0"+this.seconds : this.seconds_str = ""+this.seconds
-      this.minutes < 10 ? this.minutes_str = "0"+this.minutes : this.minutes_str = ""+this.minutes
-      this.hours < 10 ? this.hours_str = "0"+this.hours : this.hours_str = ""+this.hours
+      this.reset_variables()
+      this.convert_time()
     }
     this.isRunning = !this.isRunning
   }
 
+
+  // Wait button handler (just changes balue of isrunnign, timer is actually ticking ALL the time)
+  // It would be too inefficient to fully stop and start timer every time user clicks Wait
   waitTimer() {
     this.isRunning = !this.isRunning
   }
 
+
+  // Reset button handler (does everything that start|stop does, but does not actually stop the timer)
   resetTimer() {
-    this.seconds = 0;
-    this.minutes = 0;
-    this.hours = 0;
-    this.seconds < 10 ? this.seconds_str = "0"+this.seconds : this.seconds_str = ""+this.seconds
-    this.minutes < 10 ? this.minutes_str = "0"+this.minutes : this.minutes_str = ""+this.minutes
-    this.hours < 10 ? this.hours_str = "0"+this.hours : this.hours_str = ""+this.hours
+    this.reset_variables()
+    this.convert_time()
   }
 
 
-
-
-  half_the_time() {
+  // Universe breaking power. fully stop the timer, then change the interval value, then restart ngOnInit  which restarts the timer.
+  // All second/minute/hour values stay the same
+  change_time(time) {
     this.stopwatch.unsubscribe();
-    this.time_stamp = 2000
+    this.time_stamp = time
     this.seconds--;
     this.ngOnInit()
   }
 
-  normal_time() {
-    this.stopwatch.unsubscribe();
-    this.time_stamp = 1000
-    this.seconds--;
-    this.ngOnInit()
+
+  // rainbow mode button handler, changes the state of isRainbow which is used in the template as a render condition
+  rainbow_toggle() {
+      this.isRainbow = !this.isRainbow
   }
 
-  twice_the_time() {
-    this.stopwatch.unsubscribe();
-    this.time_stamp = 500
-    this.seconds--;
-    this.ngOnInit()
-  }
 
-  extra_time() {
-    this.stopwatch.unsubscribe();
-    this.time_stamp = 100
-    this.seconds--;
-    this.ngOnInit()
+  // function used im template to generate random color for spans
+  apply_color() {
+    return {"color" : `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`}
   }
 
 }
